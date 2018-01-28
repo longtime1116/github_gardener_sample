1715d193cc3dfa730edadb88453c1ce4bb2d91ea
longtime1116 <longtime1116@gmail.com>
2018-01-28 22:09:49 +0900

    try to read readable stream but I gave up...(comment out)

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@
 
 <script>
 import myheader from './components/myheader';
-
 export default {
   components: {
     myheader,
@@ -29,10 +28,20 @@ export default {
       this.msg = '';
     },
     getGarden() {
-      console.log('hoge');
       fetch(`http://localhost:8081/getGarden.php?url=https://github.com/users/${this.msg}/contributions`)
         .then((response) => {
           console.log(response);
+          // let reader = response.body.getReader();
+          // let chunk = 0;
+
+          // reader.read().then(function processResult(result) {
+          //   if (result.done) {
+          //     console.log("complete")
+          //     return "Fetch complete"
+          //   }
+          //   console.log(result.value);
+          //   return reader.read().then(processResult)
+          // })
           return response.body;
         }).then((data) => {
           console.log(data);
